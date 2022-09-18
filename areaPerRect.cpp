73868e7018cc5db3df77f8101a698760fc6cec6a
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2022-09-18 21:58:26 +0000

    Added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,13 @@
+// Created by: Spencer.Scarlett
+// Created on: Sept 18, 2022
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 10cm X 4cm.
+#include <iostream>
+
+int main() {
+    std::cout << "A rectangle has the dimensions of\n";
+    std::cout << "length of 10cm and width of 4cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 10 * 4 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (10 + 4) << " cm\n";
+}
---
+++ basicMath.cpp
@@ -0,0 +1,14 @@
+// Created by: Spencer.S
+// Created on: Sept 18, 2022
+// The program does basic math but in C++!
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "5 + 3 = " << 5 + 3 << std::endl;
+    std::cout << "9 - 6 = " << 9 - 6 << std::endl;
+    std::cout << "6 / 3 = " << 6 / 3 << std::endl;
+    std::cout << "6 - 3 / 2 = " << 6 - 3 / 2 << std::endl;
+    std::cout << "6^2 = " << pow(6, 2) << std::endl;
+    std::cout << "4 / 2 = " << float(4) / float(2) << std::endl;
+}
